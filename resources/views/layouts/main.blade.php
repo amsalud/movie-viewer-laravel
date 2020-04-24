342f04c91267f17fee36cc6568242c11955bb693
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-24 15:23:19 -0400

    Add avatar image

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -30,6 +30,11 @@
                         <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
                     </div>
                 </div>
+                <div class="ml-4">
+                    <a href="">
+                        <img src="/img/avatar.jpg" alt="avatar" class="rounded-full w-8 h-8">
+                    </a>
+                </div>
             </div>
         </div>
     </nav>
