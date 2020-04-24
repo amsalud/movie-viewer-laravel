d38306b6290ad028ae7f1c2b6011eb39fd6b5de5
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-24 15:17:13 -0400

    Add searchbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -23,6 +23,14 @@
                     <a href="#" class="hover:text-gray-300">Actors</a>
                 </li>
             </ul>
+            <div class="flex items-center">
+                <div class="relative">
+                    <input type="text" class="bg-gray-800 rounded-full w-64 px-4 pl-8 py-1 text-sm focus:outline-none focus:shadow-outline" placeholder="Search">
+                    <div class="absolute top-0">
+                        <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
+                    </div>
+                </div>
+            </div>
         </div>
     </nav>
     @yield('content')
