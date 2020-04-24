8a4e60ce7809d4f94f39a1de2223ceffdeb0c9e2
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-24 15:25:25 -0400

    Add index heading

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.main')
 
 @section('content')
-    <div>Index of movies</div>
+    <div class="container mx-auto px-4 pt-16">
+        <div class="popular-movies">
+            <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Popular Movies</h2>
+        </div>
+    </div>
 @endsection
