75e3ec9cc6feb393b6eee5e85a83f468625880e6
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-24 20:10:03 -0400

    Add movie images gallery

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -57,4 +57,41 @@
             </div>
         </div>
     </div>
+    <div class="movie-images">
+        <div class="container mx-auto px-4 py-16">
+            <h2 class="text-4xl font-semibold">Images</h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/image1.jpg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/image2.jpg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/image3.jpg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/image4.jpg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/image5.jpg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/image6.jpg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
