0370073cb699b3fcb901189fb4e7f285c6944629
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-25 11:30:57 -0400

    Add routes to fetch movies

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', 'MoviesController@index')->name('movies.index');
+Route::get('/movies/{movie}', 'MoviesContoller@show')->name('movies.show');
+
 Route::view('/', 'index');
 Route::view('/movie', 'show');
