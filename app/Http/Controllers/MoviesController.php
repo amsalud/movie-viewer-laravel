20932790f0d936d4b7fc336c45d8aacda7c0cdce
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-25 12:34:14 -0400

    Fetch now playing movies

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -15,15 +15,14 @@ class MoviesController extends Controller
     public function index()
     {
         $popularMovies = Http::withToken(config('services.tmdb.token'))->get('https://api.themoviedb.org/3/movie/popular')->json()['results'];
+        $nowPlayingMovies = Http::withToken(config('services.tmdb.token'))->get('https://api.themoviedb.org/3/movie/now_playing')->json()['results'];
         $genreArray = Http::withToken(config('services.tmdb.token'))->get('https://api.themoviedb.org/3/genre/movie/list')->json()['genres'];
 
         $genres = collect($genreArray)->mapWithKeys(function($genre){
             return [ $genre['id'] => $genre['name']];
         });
 
-        dump($popularMovies, $genres);
-
-        return view('index', ['popularMovies' => $popularMovies, 'genres' => $genres]);
+        return view('index', ['popularMovies' => $popularMovies, 'nowPlayingMovies' => $nowPlayingMovies, 'genres' => $genres]);
     }
 
     /**
