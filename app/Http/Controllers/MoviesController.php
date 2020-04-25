d6222caf699c8abf2926efa85bbbc0ee528b0035
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-25 12:20:16 -0400

    Fetch popular movies and genre data

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class MoviesController extends Controller
 {
@@ -13,7 +14,16 @@ class MoviesController extends Controller
      */
     public function index()
     {
-        //
+        $popularMovies = Http::withToken(config('services.tmdb.token'))->get('https://api.themoviedb.org/3/movie/popular')->json()['results'];
+        $genreArray = Http::withToken(config('services.tmdb.token'))->get('https://api.themoviedb.org/3/genre/movie/list')->json()['genres'];
+
+        $genres = collect($genreArray)->mapWithKeys(function($genre){
+            return [ $genre['id'] => $genre['name']];
+        });
+
+        dump($popularMovies, $genres);
+
+        return view('index', ['popularMovies' => $popularMovies, 'genres' => $genres]);
     }
 
     /**
