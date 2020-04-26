96424d4a1ccbd3520502a9f0b00f1357522d4f11
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-26 13:27:30 -0400

    Display search results

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,7 +1,18 @@
 <div class="relative mt-3 md:ml-4 md:mt-0">
-    <input type="text" class="bg-gray-800 rounded-full w-64 px-4 pl-8 py-1 text-sm focus:outline-none focus:shadow-outline" placeholder="Search">
+    <input wire:model.debounce.500ms="search" type="text" class="bg-gray-800 rounded-full w-64 px-4 pl-8 py-1 text-sm focus:outline-none focus:shadow-outline" placeholder="Search">
     <div class="absolute top-0">
         <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24">
             <path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z" /></svg>
     </div>
+    @if(isset($searchResults))
+    <div class="absolute bg-gray-800 rounded w-64 mt-4">
+        <ul>
+            @foreach($searchResults as $searchResult)
+            <li class="border-b text-sm border-gray-700">
+                <a href="#" class="block hover:bg-gray-700 px-3 py-3">{{$searchResult['title']}}</a>
+            </li>
+            @endforeach
+        </ul>    
+    </div>
+    @endif
 </div>
