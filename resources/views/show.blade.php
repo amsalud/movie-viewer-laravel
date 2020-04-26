71f81f16bb5f530e2d74f9720410a8afe3a61bd4
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-26 15:06:13 -0400

    Display trailer in the modal

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -31,12 +31,11 @@
                 <h4 class="text-white font-semibold">Featured Crew</h4>
                 <div class="flex mt-4">
                     @foreach($movie['credits']['crew'] as $crew)
-                    @if($loop->index < 2) 
-                    <div class="mr-8">
+                    @if($loop->index < 2) <div class="mr-8">
                         <div>{{ $crew['name'] }}</div>
                         <div class="text-sm text-gray-400">{{$crew['job']}}</div>
-                    </div>
-                    @endif
+                </div>
+                @endif
                 @endforeach
             </div>
         </div>
@@ -49,6 +48,21 @@
                 <span class="ml-2">Play Trailer</span>
             </a>
         </div>
+        <div style="background-color: rgba(0, 0, 0, .5);" class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto" x-show.transition.opacity="isOpen">
+            <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+                <div class="bg-gray-900 rounded">
+                    <div class="flex justify-end pr-4 pt-2">
+                        <button @click="isOpen = false" @keydown.escape.window="isOpen = false" class="text-3xl leading-none hover:text-gray-300">&times;
+                        </button>
+                    </div>
+                    <div class="modal-body px-8 py-8">
+                        <div class="responsive-container overflow-hidden relative" style="padding-top: 56.25%">
+                            <iframe class="responsive-iframe absolute top-0 left-0 w-full h-full" src="https://www.youtube.com/embed/{{ $movie['videos']['results'][0]['key'] }}" style="border:0;" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         @endif
     </div>
 </div>
@@ -58,8 +72,7 @@
         <h2 class="text-4xl font-seminold">Cast</h2>
         <div class="grid grid-cols-1 sm:grid-cols2 md:grid-cols-3 lg:grid-cols-5 gap-8">
             @foreach($movie['credits']['cast'] as $cast)
-            @if($loop->index < 5) 
-            <div class="mt-8">
+            @if($loop->index < 5) <div class="mt-8">
                 <a href="{{ (isset($cast['profile_path']) ? 'https://image.tmdb.org/t/p/w300' . $cast['profile_path'] : 'https://api.adorable.io/avatars/285/abott@adorable.png' )  }}" target="_blank">
                     <img src="{{ (isset($cast['profile_path']) ? 'https://image.tmdb.org/t/p/w300' . $cast['profile_path'] : 'https://api.adorable.io/avatars/285/abott@adorable.png' )  }}" alt="{{$cast['name']}}" class="hover:opacity-75 transition-ease-in-out duration-150">
                 </a>
@@ -67,26 +80,25 @@
                     <a href="#" class="text-lg mt-2 hover:text-gray:300">{{$cast['name']}}</a>
                     <div class="text-sm text-gray-400">{{$cast['character']}}</div>
                 </div>
-            </div>
-            @endif
-            @endforeach
         </div>
+        @endif
+        @endforeach
     </div>
+</div>
 </div>
 <div class="movie-images">
     <div class="container mx-auto px-4 py-16">
         <h2 class="text-4xl font-semibold">Images</h2>
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
             @foreach($movie['images']['backdrops'] as $backdrop)
-            @if($loop->index < 6)
-            <div class="mt-8">
+            @if($loop->index < 6) <div class="mt-8">
                 <a href="{{'https://image.tmdb.org/t/p/w300' . $backdrop['file_path']}}" target="_blank">
                     <img src="{{'https://image.tmdb.org/t/p/w300' . $backdrop['file_path']}}" alt="image" class="hover:opacity-75 transition ease-in-out duration-150">
                 </a>
-            </div>
-            @endif
-            @endforeach
         </div>
+        @endif
+        @endforeach
     </div>
+</div>
 </div>
 @endsection
