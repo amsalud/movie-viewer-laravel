065cba17e1d4c560091559e9abe6e23f63793bb2
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-27 19:50:56 -0400

    Define movies view model properties

--- app/ViewModels/MoviesViewModel.php
+++ app/ViewModels/MoviesViewModel.php
@@ -6,8 +6,26 @@ use Spatie\ViewModels\ViewModel;
 
 class MoviesViewModel extends ViewModel
 {
-    public function __construct()
+    private $popularMovies;
+    private $nowPlayingMovies;
+    private $genres;
+
+    public function __construct($popularMovies, $nowPlayingMovies, $genres)
     {
-        //
+        $this->popularMovies = $popularMovies;
+        $this->nowPlayingMovies = $nowPlayingMovies;
+        $this->genres = $genres;
+    }
+
+    public function getPopularMovies(){
+        return $this->popularMovies;
+    }
+
+    public function getNowPlayingMovies(){
+        return $this->nowPlayingMovies;
+    }
+
+    public function getGenres(){
+        return $this->genres;
     }
 }
