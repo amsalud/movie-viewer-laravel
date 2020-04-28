f0f007ada3f0b465936b699a3f063b84bc012b78
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-27 20:32:09 -0400

    Use MovieViewModel

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\ViewModels\MoviesViewModel;
+use App\ViewModels\MovieViewModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
@@ -54,7 +55,10 @@ class MoviesController extends Controller
     public function show($id)
     {
         $movie = Http::withToken(config('services.tmdb.token'))->get('https://api.themoviedb.org/3/movie/' . $id . '?append_to_response=credits,videos,images')->json();
-        return view('show', ['movie' => $movie]);
+        
+        $viewModel = new MovieViewModel($movie);
+        
+        return view('show', $viewModel);
     }
 
     /**
