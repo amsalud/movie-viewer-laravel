bdae36e9fd5c970e4bd8c28aeeb917caee06a5c2
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-27 21:51:47 -0400

    Create index.blade.php

---
+++ resources/views/actors/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+
+@section('content')
+<div class="container mx-auto px-4 pt-16">
+    <div class="popular-actors">
+        <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Popular Actors</h2>
+        <div class="grid grid-cols-1 sm:grid-cols2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+            <div>Actor Info</div>
+        </div>
+    </div>
+</div>
+@endsection
