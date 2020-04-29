64b2563cff7d2f1a282262846a94ef7e1db5baf2
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-29 18:40:11 -0400

    Add image placeholder if movie poster_path is not set

--- app/ViewModels/MovieViewModel.php
+++ app/ViewModels/MovieViewModel.php
@@ -17,7 +17,7 @@ class MovieViewModel extends ViewModel
     public function movie(){
         return collect($this->movie)->merge([
             'title' => $this->movie['title'] . ' (' . Carbon::parse($this->movie['release_date'])->format('Y') . ')',
-            'poster_path' => 'https://image.tmdb.org/t/p/w500' . $this->movie['poster_path'],
+            'poster_path' => isset($this->movie['poster_path']) ? 'https://image.tmdb.org/t/p/w500' . $this->movie['poster_path'] : 'https://via.placeholder.com/500x450',
             'vote_average' => $this->movie['vote_average'] * 10 . '%',  
             'release_date' => Carbon::parse($this->movie['release_date'])->format('M d, Y'),
             'trailer' => count($this->movie['videos']['results']) > 0 ? 'https://www.youtube.com/embed/' . ($this->movie['videos']['results'][0]['id']) : null,
