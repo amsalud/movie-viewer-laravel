9115a56d17349f6eed0d27838f8fe3d392cdd2c4
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-28 21:01:00 -0400

    Add data formatting to known_for

--- app/ViewModels/ActorsViewModel.php
+++ app/ViewModels/ActorsViewModel.php
@@ -21,7 +21,8 @@ class ActorsViewModel extends ViewModel
         return collect($actors)->map(function($actor){
             return collect($actor)->merge([
                 'profile_path' => $actor['profile_path'] ? 'https://image.tmdb.org/t/p/w235_and_h235_face' . $actor['profile_path'] : 'https://ui-avatars.com/api/?size=235&name=' . $actor['name'],
-            ])->only(['profile_path', 'name', 'id']);
+                'known_for' => collect($actor['known_for'])->where('media_type', 'movie')->pluck('title')->union(collect($actor['known_for'])->where('media_type', 'tv')->pluck('name'))->implode(', ')
+            ])->only(['profile_path', 'name', 'id', 'known_for']);
         });
     }
 }
