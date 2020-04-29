cc18734d0a6a1d50b7abaf40612e3c0e94dd59d0
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-28 20:24:03 -0400

    Add actor item ui layout

--- resources/views/actors/index.blade.php
+++ resources/views/actors/index.blade.php
@@ -5,7 +5,15 @@
     <div class="popular-actors">
         <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Popular Actors</h2>
         <div class="grid grid-cols-1 sm:grid-cols2 md:grid-cols-3 lg:grid-cols-5 gap-8">
-            <div>Actor Info</div>
+            <div class="actor mt-8">
+                <a href="#">
+                    <img src="/img/actor1.jpg" alt="" alt="profile image" class="hover:opacity-75 transition ease-in-out duration-150"/>
+                </a>
+                <div class="mt-2">
+                    <a href="" class="text-lg hover:text-gray-300">Robert Downey Jr.</a>
+                    <div class="text-sm truncate text-gray-400">Iron Man, Avengers, Avengers: Infinity War</div>
+                </div> 
+            </div>
         </div>
     </div>
 </div>
