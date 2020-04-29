d76f34c609e6fcc547e167082e7f7d280e1a17fc
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-29 17:45:24 -0400

    Create TvShowsViewModel.php

---
+++ app/ViewModels/TvShowsViewModel.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\ViewModels;
+
+use Carbon\Carbon;
+use Spatie\ViewModels\ViewModel;
+
+class TvShowsViewModel extends ViewModel
+{
+    public $popularTvShows;
+    public $genres;
+
+    public function __construct($popularTvShows, $genres)
+    {
+        $this->popularTvShows = $popularTvShows;
+        $this->genres = $genres;
+    }
+
+    public function popularTvShows(){
+        return $this->formatTvShowsData($this->popularTvShows);
+    }
+    private function formatTvShowsData($tvShows){
+        return collect($tvShows)->map(function($tvShow){
+            return collect($tvShow)->merge([
+                'poster_path' => 'https://image.tmdb.org/t/p/w500' . $tvShow['poster_path'],
+                'vote_average' => $tvShow['vote_average'] * 10 . '%',  
+                'first_air_date' => Carbon::parse($tvShow['first_air_date'])->format('M d, Y'),
+                'genres' => $this->formatGenres($tvShow)
+            ])->only(['poster_path', 'id', 'name', 'vote_average', 'overview', 'first_air_date', 'genres']);
+        });
+    }
+
+    private function formatGenres($tvShow){
+        return collect($tvShow['genre_ids'])->mapWithKeys(function($value){
+            return [$value => $this->genres()->get($value)];
+        })->implode(', ');
+    }
+
+    public function genres(){
+        return collect($this->genres)->mapWithKeys(function($genre){
+            return [ $genre['id'] => $genre['name']];
+        });
+    }
+}
