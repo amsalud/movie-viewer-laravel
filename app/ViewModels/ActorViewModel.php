a2fde2b5498a4ddce873eeb9cef873022d267b4e
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-28 22:24:50 -0400

    Add data formatting

--- app/ViewModels/ActorViewModel.php
+++ app/ViewModels/ActorViewModel.php
@@ -2,12 +2,24 @@
 
 namespace App\ViewModels;
 
+use Carbon\Carbon;
 use Spatie\ViewModels\ViewModel;
 
 class ActorViewModel extends ViewModel
 {
-    public function __construct()
+    public $actor;
+
+    public function __construct($actor)
     {
-        //
+        $this->actor = $actor;
+    }
+
+    public function actor(){
+        return collect($this->actor)->merge([
+            'profile_path' => 'https://image.tmdb.org/t/p/w150_and_h225_bestv2'. $this->actor['profile_path'],
+            'birthday' =>  Carbon::parse($this->actor['birthday'])->format('M d, Y'),
+            'age' => Carbon::parse($this->actor['birthday'])->age
+
+        ])->dump();
     }
 }
