f7aaa262f10b4b13299c28868bb070f4029d1373
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-29 18:37:30 -0400

    Add check if casts and images are empty

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -64,6 +64,7 @@
         </div>
     </div>
 </div>
+@if(count($movie['casts']) > 0)
 <div class="movie-cast border-b border-gray-800" x-data="{ isOpen: false, image: ''}">
     <div class="container mx-auto px-4 py-16">
         <h2 class="text-4xl font-seminold">Cast</h2>
@@ -79,10 +80,11 @@
                 </div>
             </div>
             @endforeach
+        </div>
     </div>
 </div>
-</div>
-
+@endif
+@if(count($movie['images']) > 0)
 <div class="movie-images" x-data="{ isOpen: false, image: ''}">
     <div class="container mx-auto px-4 py-16">
         <h2 class="text-4xl font-semibold">Images</h2>
@@ -95,23 +97,24 @@
                                 " href="#">
                     <img src="{{ $image }}" alt="image1" class="hover:opacity-75 transition ease-in-out duration-150">
                 </a>
+            </div>
+            @endforeach
         </div>
-        @endforeach
-    </div>
 
-    <div style="background-color: rgba(0, 0, 0, .5);" class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto" x-show="isOpen">
-        <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
-            <div class="bg-gray-900 rounded">
-                <div class="flex justify-end pr-4 pt-2">
-                    <button @click="isOpen = false" @keydown.escape.window="isOpen = false" class="text-3xl leading-none hover:text-gray-300">&times;
-                    </button>
-                </div>
-                <div class="modal-body px-8 py-8">
-                    <img :src="image" alt="poster">
+        <div style="background-color: rgba(0, 0, 0, .5);" class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto" x-show="isOpen">
+            <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+                <div class="bg-gray-900 rounded">
+                    <div class="flex justify-end pr-4 pt-2">
+                        <button @click="isOpen = false" @keydown.escape.window="isOpen = false" class="text-3xl leading-none hover:text-gray-300">&times;
+                        </button>
+                    </div>
+                    <div class="modal-body px-8 py-8">
+                        <img :src="image" alt="poster">
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-</div>
+@endif
 @endsection
