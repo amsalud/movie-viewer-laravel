3b8c5c31dd9c2a737aa54cf931fbc37ccc495daf
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-28 21:43:52 -0400

    Add link to actor detail page

--- resources/views/actors/index.blade.php
+++ resources/views/actors/index.blade.php
@@ -8,11 +8,11 @@
 
             @foreach($popularActors as $actor)
             <div class="actor mt-8">
-                <a href="#">
+                <a href="{{ route('actors.show', $actor['id']) }}">
                     <img src="{{$actor['profile_path']}}" alt="" alt="profile image" class="hover:opacity-75 transition ease-in-out duration-150" />
                 </a>
                 <div class="mt-2">
-                    <a href="" class="text-lg hover:text-gray-300">{{$actor['name']}}</a>
+                    <a href="{{ route('actors.show', $actor['id']) }}" class="text-lg hover:text-gray-300">{{$actor['name']}}</a>
                     <div class="text-sm truncate text-gray-400">{{ $actor['known_for']}}</div>
                 </div>
             </div>
