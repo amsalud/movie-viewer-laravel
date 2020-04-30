d75ab5377056c6c2af016d823c7492fe10442af0
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-30 01:24:04 -0400

    Add method to format search results

--- app/Http/Livewire/SearchDropdown.php
+++ app/Http/Livewire/SearchDropdown.php
@@ -14,9 +14,29 @@ class SearchDropdown extends Component
         $searchResults = [];
 
         if(strlen($this->search) > 2){
-            $searchResults = Http::withToken(config('services.tmdb.token'))->get('https://api.themoviedb.org/3/search/movie?query='. $this->search)->json()['results'];
+            $searchResults = Http::withToken(config('services.tmdb.token'))->get('https://api.themoviedb.org/3/search/multi?query='. $this->search)->json()['results'];
         }
 
-        return view('livewire.search-dropdown', ['searchResults' => collect($searchResults)->take(10)]);
+        return view('livewire.search-dropdown', ['searchResults' => $this->formatSearchResults($searchResults)]);
+    }
+
+    private function formatSearchResults($searchResults){
+        return collect($searchResults)->map(function($searchResult){
+            return collect($searchResult)->merge([
+                'route_name' => $this->getRouteName($searchResult)
+            ]);
+        })->take(10)->dump();
+    }
+
+    private function getRouteName($searchResult){
+        if($searchResult['media_type'] === 'movie'){
+            return 'movies.show';
+        }
+        else if($searchResult['media_type'] === 'tv'){
+            return 'tv.show';
+        }
+        else if($searchResult['media_type'] === 'person'){
+            return 'actors.show';
+        }
     }
 }
