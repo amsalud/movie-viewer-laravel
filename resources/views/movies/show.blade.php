ae2944e829f5e4c928554315eb6e0f43ee33ee2a
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-30 13:35:45 -0400

    Render modal component

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -101,19 +101,10 @@
             @endforeach
         </div>
 
-        <div style="background-color: rgba(0, 0, 0, .5);" class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto" x-show="isOpen">
-            <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
-                <div class="bg-gray-900 rounded">
-                    <div class="flex justify-end pr-4 pt-2">
-                        <button @click="isOpen = false" @keydown.escape.window="isOpen = false" class="text-3xl leading-none hover:text-gray-300">&times;
-                        </button>
-                    </div>
-                    <div class="modal-body px-8 py-8">
-                        <img :src="image" alt="poster">
-                    </div>
-                </div>
-            </div>
-        </div>
+        <x-modal>
+            <img :src="image" alt="poster">
+        </x-modal>
+       
     </div>
 </div>
 @endif
