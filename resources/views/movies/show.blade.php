7f2f4868f2ace959e67b9319f9997d5357c86351
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-30 13:45:17 -0400

    Render modal component for movie trailers

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -44,21 +44,11 @@
                         <span class="ml-2">Play Trailer</span>
                     </button>
                 </div>
-                <div style="background-color: rgba(0, 0, 0, .5);" class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto" x-show.transition.opacity="isOpen">
-                    <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
-                        <div class="bg-gray-900 rounded">
-                            <div class="flex justify-end pr-4 pt-2">
-                                <button @click="isOpen = false" @keydown.escape.window="isOpen = false" class="text-3xl leading-none hover:text-gray-300">&times;
-                                </button>
-                            </div>
-                            <div class="modal-body px-8 py-8">
-                                <div class="responsive-container overflow-hidden relative" style="padding-top: 56.25%">
-                                    <iframe class="responsive-iframe absolute top-0 left-0 w-full h-full" src="{{ $movie['trailer']}}" style="border:0;" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                                </div>
-                            </div>
-                        </div>
+                <x-modal>
+                    <div class="responsive-container overflow-hidden relative" style="padding-top: 56.25%">
+                        <iframe class="responsive-iframe absolute top-0 left-0 w-full h-full" src="{{ $movie['trailer']}}" style="border:0;" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                     </div>
-                </div>
+                </x-modal>
                 @endif
             </div>
         </div>
