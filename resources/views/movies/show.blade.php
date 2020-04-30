33c90f418dff451369b43d670aac400b03b49ea0
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-30 14:02:23 -0400

    Render image gallery component in movie detail page

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -75,25 +75,6 @@
 </div>
 @endif
 @if(count($movie['images']) > 0)
-<div class="movie-images" x-data="{ isOpen: false, image: ''}">
-    <div class="container mx-auto px-4 py-16">
-        <h2 class="text-4xl font-semibold">Images</h2>
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-            @foreach ($movie['images'] as $image)
-            <div class="mt-8">
-                <a @click.prevent="
-                                    isOpen = true
-                                    image='{{ $image }}'
-                                " href="#">
-                    <img src="{{ $image }}" alt="image1" class="hover:opacity-75 transition ease-in-out duration-150">
-                </a>
-            </div>
-            @endforeach
-        </div>
-        <x-modal>
-            <img :src="image" alt="poster">
-        </x-modal>
-    </div>
-</div>
+    <x-image-gallery :images="$movie['images']"></x-image-gallery>
 @endif
 @endsection
