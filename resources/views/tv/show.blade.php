3c1bc148969d63609f6dbec5f5837f853b4ecfc3
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-30 13:59:14 -0400

    Render image gallery component in tv show detail page

--- resources/views/tv/show.blade.php
+++ resources/views/tv/show.blade.php
@@ -75,25 +75,6 @@
 </div>
 
 @if(count($tvShow['images']) > 0)
-<div class="tvShow-images" x-data="{ isOpen: false, image: ''}">
-    <div class="container mx-auto px-4 py-16">
-        <h2 class="text-4xl font-semibold">Images</h2>
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-            @foreach ($tvShow['images'] as $image)
-            <div class="mt-8">
-                <a @click.prevent="
-                                    isOpen = true
-                                    image='{{ $image }}'
-                                " href="#">
-                    <img src="{{ $image }}" alt="image1" class="hover:opacity-75 transition ease-in-out duration-150">
-                </a>
-            </div>
-            @endforeach
-        </div>
-        <x-modal>
-            <img :src="image" alt="poster">
-        </x-modal>
-    </div>
-</div>
+    <x-image-gallery :images="$tvShow['images']"></x-image-gallery>
 @endif
 @endsection
