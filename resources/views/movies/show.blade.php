9b250440897151b0e7573599478e68fa3ec74bd4
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-30 14:08:21 -0400

    Render cast info component in movie detail page

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -55,26 +55,9 @@
     </div>
 </div>
 @if(count($movie['casts']) > 0)
-<div class="movie-cast border-b border-gray-800" x-data="{ isOpen: false, image: ''}">
-    <div class="container mx-auto px-4 py-16">
-        <h2 class="text-4xl font-seminold">Cast</h2>
-        <div class="grid grid-cols-1 sm:grid-cols2 md:grid-cols-3 lg:grid-cols-5 gap-8">
-            @foreach($movie['casts'] as $cast)
-            <div class="mt-8">
-                <a href="{{ route('actors.show', $cast['id']) }}">
-                    <img src="{{ $cast['image'] }}" alt="{{$cast['name']}}" class="hover:opacity-75 transition-ease-in-out duration-150">
-                </a>
-                <div class="mt-2">
-                    <a href="{{ route('actors.show', $cast['id']) }}" class="text-lg mt-2 hover:text-gray:300">{{$cast['name']}}</a>
-                    <div class="text-sm text-gray-400">{{$cast['character']}}</div>
-                </div>
-            </div>
-            @endforeach
-        </div>
-    </div>
-</div>
+    <x-cast-info :casts="$movie['casts']"/>
 @endif
 @if(count($movie['images']) > 0)
-    <x-image-gallery :images="$movie['images']"></x-image-gallery>
+    <x-image-gallery :images="$movie['images']" />
 @endif
 @endsection
