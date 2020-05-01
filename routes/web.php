70f4286654b2c109adc69344fe74de0e96599037
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-01 00:30:30 -0400

    Add movies route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'MoviesController@index')->name('movies.index');
+Route::get('/movies', 'MoviesController@index')->name('movies.index');
 Route::get('/movies/{id}', 'MoviesController@show')->name('movies.show');
 
 Route::get('/actors', 'ActorsController@index')->name('actors.index');
